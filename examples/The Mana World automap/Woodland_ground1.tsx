07d0e21d992ac052f65cd24322d823275731ddc4
Stefan Beller <stefanbeller@googlemail.com>
2012-01-13 00:18:39 +0100

    The Mana World examples for automapping

---
+++ examples/The Mana World automap/Woodland_ground1.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset name="Woodland_ground" tilewidth="32" tileheight="32">
+ <image source="Woodland_ground.png" width="192" height="96"/>
+</tileset>
