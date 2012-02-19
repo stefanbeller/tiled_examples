d29b65f4a059590147f640fe0d9589612acc6976
Stefan Beller <stefanbeller@googlemail.com>
2012-02-19 20:20:18 +0100

    adding another explanation of the inputsection

--- docs/Automapping/build.sh
+++ docs/Automapping/build.sh
@@ -1,2 +1,9 @@
 latex Automapping_rulefiles.tex
+latex Automapping_rulefiles.tex
+latex Automapping_rulefiles.tex
 dvipdfm Automapping_rulefiles.dvi
+
+rm Automapping_rulefiles.aux
+rm Automapping_rulefiles.log
+rm Automapping_rulefiles.dvi
+
